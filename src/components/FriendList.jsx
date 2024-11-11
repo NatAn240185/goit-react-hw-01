22b879490e426f9cc7208743aa7b69178e24a738
Natalia Andryan <168345429+NatAn240185@users.noreply.github.com>
2024-11-11 12:05:09 +0100

    add all

---
+++ src/components/FriendList.jsx
@@ -0,0 +1,18 @@
+import styles from './FriendList.module.css'
+import FriendListItem from './FriendListItem'
+
+const FriendList = ({ friends }) => {
+    return (<ul className={styles.listOfFriends}>
+        {friends.map((friend) => (
+            <li key={friend.id}>
+                <FriendListItem
+                    avatar={friend.avatar}
+                    name={friend.name}
+                    isOnline={friend.isOnline} />
+            </li>
+        ))}
+    </ul>
+    );
+};
+
+export default FriendList;
